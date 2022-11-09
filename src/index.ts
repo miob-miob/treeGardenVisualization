b385f2dec0a0e789cf4ee3a31a308a5fd240d483
oborim01 <michal.oboril@dixonscarphone.com>
2022-11-09 19:19:09 +0100

    Sibgle tree is not working

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+
+
 export { Board } from './components/MainBoard';
 export { UfoComponent } from './components/Ufo';
-
+export { TreeVisualization } from './components/TreesVisualization/TreeVisualization';
